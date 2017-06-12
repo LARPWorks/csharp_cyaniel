97bade85e844f24cea123439e31e8f392eb00c39
biscuitWizard <reiv3r@gmail.com>
2017-06-12 06:12:26 -0700

    Added in some default content and made the homepage and default layout. Partial conversion from FLASK pages to Razor.
    Moved project to feature-based project layout.

---
+++ LARPWorks.Cyaniel.Web/Modules/Home/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace LARPWorks.Cyaniel.Web.Modules.Home
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters => View["Index.cshtml"];
+        }
+    }
+}
