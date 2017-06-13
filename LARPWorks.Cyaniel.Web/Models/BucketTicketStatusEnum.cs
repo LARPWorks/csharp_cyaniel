2cdcab04099e6476aeeab691ede1d3e617ff3459
biscuitWizard <reiv3r@gmail.com>
2017-06-13 15:11:54 -0700

    Added some beginnings to the bucket system.

---
+++ LARPWorks.Cyaniel.Web/Models/BucketTicketStatusEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace LARPWorks.Cyaniel.Web.Models
+{
+    public enum BucketTicketStatusEnum : ushort
+    {
+        Unclaimed = 0,
+        RequiresAttention = 1,
+        InProgress = 2,
+        Complete = 3
+    }
+}
