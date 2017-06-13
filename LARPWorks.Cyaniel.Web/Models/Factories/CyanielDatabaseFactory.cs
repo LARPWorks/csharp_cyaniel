4661034065a1b691a81626751afa42f517646c39
biscuitWizard <reiv3r@gmail.com>
2017-06-13 00:36:21 -0700

    Added some database tie-ins to the registration module.

---
+++ LARPWorks.Cyaniel.Web/Models/Factories/CyanielDatabaseFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LARPWorks.Cyaniel.Web.Models.Factories
+{
+    public class CyanielDatabaseFactory : IDbFactory
+    {
+        public PetaPoco.Database Create()
+        {
+            return new PetaPoco.Database("MySQL");
+        }
+    }
+}
