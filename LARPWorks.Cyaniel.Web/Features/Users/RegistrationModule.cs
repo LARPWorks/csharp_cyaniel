e7f6530e8c8ad8ce14d4577292fa25ef70308215
biscuitWizard <reiv3r@gmail.com>
2017-06-12 23:09:50 -0700

    Fixing log dereference error in branch.

--- LARPWorks.Cyaniel.Web/Features/Users/RegistrationModule.cs
+++ LARPWorks.Cyaniel.Web/Features/Users/RegistrationModule.cs
@@ -1,5 +1,7 @@
 ﻿using LARPWorks.Cyaniel.Web.Features.SharedViews;
+using MySQL;
 using Nancy;
+using Nancy.ModelBinding;
 
 namespace LARPWorks.Cyaniel.Web.Features.Users
 {
@@ -7,7 +9,40 @@ namespace LARPWorks.Cyaniel.Web.Features.Users
     {
         public RegistrationModule() : base("Users")
         {
-            Get["/register"] = parameters => View["Register.cshtml", new BaseCyanielViewModel()];
+            var baseCyanielViewModel = new BaseCyanielViewModel();
+            Get["/register"] = parameters => View["Register.cshtml", baseCyanielViewModel];
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+            Post["/register", true] = async (x, ct) =>
+            {
+                User newUser = this.Bind<User>();
+                var validationError = "";
+                var failedValidation = false;
+
+                if (string.IsNullOrEmpty(newUser.Username))
+                {
+                    failedValidation = true;
+                    validationError += string.Format("Must provide a username!<br>");
+                }
+
+                if (failedValidation)
+                {
+                    ViewBag.ValidationError = validationError;
+                    return View["Register.cshtml", baseCyanielViewModel];
+                }
+
+                return View["Register.cshtml", baseCyanielViewModel];
+
+                //var accountName = Request.Form.AccountName;
+                //var password = Request.Form.Password;
+                //var password2 = Request.Form.Password2;
+
+                //if (password != password2) {
+
+                //}
+                //return View["Register.cshtml", baseCyanielViewModel];
+            };
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         }
     }
 }
