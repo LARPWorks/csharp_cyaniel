3b00a036b2b8d9e09c82a440a3eaf62881c47ad3
biscuitWizard <reiv3r@gmail.com>
2017-06-13 20:29:10 -0700

    Added in the basics for a character sheet tabbed interface.

---
+++ LARPWorks.Cyaniel.Web/Features/Characters/CharacterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using LARPWorks.Cyaniel.Web.Features.SharedViews;
+
+namespace LARPWorks.Cyaniel.Web.Features.Characters
+{
+    public class CharacterViewModel : BaseCyanielViewModel
+    {
+        public string[] Sections { get; set; }
+
+        public CharacterViewModel()
+        {
+            Sections = new []
+            {
+                "Character Sheet",
+                "Attributes & Skills",
+                "Perks & Flaws",
+                "Foci & Languages",
+                "Maneuvers",
+                "Priest Rituals",
+                "Magic",
+                "Knight Rank & Oaths",
+                "Paladin Powers",
+                "Leadership",
+                "Biography",
+                "Notes"
+            };
+        }
+    }
+}
