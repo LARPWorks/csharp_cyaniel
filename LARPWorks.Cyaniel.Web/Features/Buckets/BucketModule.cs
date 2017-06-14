f710e4c7ae1742a811841bb1fdfa1cf42b8bc187
biscuitWizard <reiv3r@gmail.com>
2017-06-13 19:10:04 -0700

    Added a webpage to create tickets.

--- LARPWorks.Cyaniel.Web/Features/Buckets/BucketModule.cs
+++ LARPWorks.Cyaniel.Web/Features/Buckets/BucketModule.cs
@@ -1,16 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LARPWorks.Cyaniel.Web.Features.Buckets;
+using LARPWorks.Cyaniel.Web.Models.Factories;
 
 namespace LARPWorks.Cyaniel.Web.Features.Bucket
 {
     public class BucketModule : CyanielModule
     {
-        public BucketModule() : base("Buckets")
+        private readonly IDbFactory _dbFactory;
+        public BucketModule(IDbFactory dbFactory) : base("Buckets")
         {
+            _dbFactory = dbFactory;
+
             Get["/index"] = parameters => View["Index.cshtml", GetViewModel<IndexViewModel>()];
+            Get["/create"] = parameters => View["Create.cshtml", BuildModel()];
+            Post["/create"] = parameters =>
+            {
+                return View["Create.cshtml", GetViewModel<CreateViewModel>()];
+            };
+        }
+
+        private CreateViewModel BuildModel()
+        {
+            var baseModel = GetViewModel<CreateViewModel>();
+            using (var db = _dbFactory.Create())
+            {
+                var buckets = db.Fetch<MySQL.Bucket>("SELECT * FROM Buckets");
+                if (buckets == null)
+                {
+                    return baseModel;
+                }
+
+                baseModel.BucketChoices = buckets.ToArray();
+            }
+
+            return baseModel;
         }
     }
 }
