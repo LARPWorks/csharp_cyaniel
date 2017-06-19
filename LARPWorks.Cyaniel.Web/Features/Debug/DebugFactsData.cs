c44e0a223679ed188715bc1a7253b2c15992397d
biscuitWizard <reiv3r@gmail.com>
2017-06-18 23:47:04 -0700

    Adding new debug page for debug-only compiles that will populate or blow away the database.

---
+++ LARPWorks.Cyaniel.Web/Features/Debug/DebugFactsData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace LARPWorks.Cyaniel.Features.Debug
+{
+#if DEBUG
+    public class DebugFactsData
+    {
+        public Dictionary<string, string[]> Facts;
+        public Dictionary<string, string[]> AdvancementLists;
+        public Dictionary<string, string[]> SocialStatusSkills;
+        public Dictionary<string, string[]> CultureSkills;
+        public Dictionary<string, string[]> EsotericGates;
+    }
+#endif
+}
---
+++ LARPWorks.Cyaniel.Web/Features/Debug/DebugModule.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using LARPWorks.Cyaniel.Features.SharedViews;
+using LARPWorks.Cyaniel.Models;
+using LARPWorks.Cyaniel.Models.Factories;
+using Nancy;
+using Nancy.ModelBinding;
+using Newtonsoft.Json;
+
+namespace LARPWorks.Cyaniel.Features.Debug
+{
+#if DEBUG
+    public class DebugModule : CyanielModule
+    {
+        public DebugModule(IDbFactory dbFactory) : base("Debug")
+        {
+            Get["/"] = paramaters => Response.AsRedirect("/Debug/Index");
+            Get["/Index"] = parameters => View["Index.cshtml", GetViewModel<BaseCyanielViewModel>()];
+            Post["/populate_db"] = parameters =>
+            {
+                var model = this.Bind<BaseCyanielViewModel>();
+
+                // Load the JSON facts..
+                var assembly = Assembly.GetExecutingAssembly();
+                DebugFactsData data;
+                using (var reader = new StreamReader(assembly.GetManifestResourceStream("LARPWorks.Cyaniel.Features.Debug.FactsData.json")))
+                {
+                    string json = reader.ReadToEnd();
+                    data = JsonConvert.DeserializeObject<DebugFactsData>(json);
+                }
+                try
+                {
+                    using (var db = dbFactory.Create())
+                    {
+                        // This is just an internal cache of how the inserts go
+                        // for all facts. It speeds up the rest of the creation process.
+                        Dictionary<string,int> factMappings = new Dictionary<string, int>();
+
+                        // Insert fact types and facts...
+                        foreach (var factType in data.Facts.Keys)
+                        {
+                            ulong primaryKey = (ulong)db.Insert(new FactType {Name = factType});
+                            foreach (var fact in data.Facts[factType])
+                            {
+                                var factKey = (ulong) db.Insert(new Fact {FactTypeId = (int) primaryKey, Name = fact});
+                                factMappings.Add(fact, (int)factKey);
+                            }
+                        }
+
+                        // Insert all the advancement lists...
+                        foreach (var advancementList in data.AdvancementLists.Keys)
+                        {
+                            ulong primaryKey = (ulong) db.Insert(new AdvancementList {Name = advancementList});
+                            foreach (var fact in data.AdvancementLists[advancementList])
+                            {
+                                var factKey = factMappings[fact];
+                                db.Insert(new AdvancementListFact
+                                {
+                                    AdvancementListId = (int) primaryKey,
+                                    FactId = factKey,
+                                    IsStaffOnly = false
+                                });
+                            }
+                        }
+
+                        // Working on adding all the 'modifiers' in the advancement lists
+                        // for all social statuses.
+                        foreach (var socialStatus in data.SocialStatusSkills.Keys)
+                        {
+                            var factKey = factMappings[socialStatus];
+                            foreach (var skill in data.SocialStatusSkills[socialStatus])
+                            {
+                                var skillKey = factMappings[skill];
+                                var advancementListFactKey =
+                                    db.Single<AdvancementListFact>(
+                                        "SELECT * FROM AdvancementListFacts WHERE FactId=@0 AND AdvancementListId=@1", skillKey,
+                                        (int)AdvancementListEnum.SocialStatusSkills).Id;
+
+                                db.Insert(new AdvancementListFactModifier
+                                {
+                                    AdvancementListFactId = advancementListFactKey,
+                                    FactRequirementId = factKey
+                                });
+                            }
+                        }
+
+                        // Working on adding all the 'modifiers' in the advancement lists
+                        // for all cultures
+                        foreach (var culture in data.CultureSkills.Keys)
+                        {
+                            var factKey = factMappings[culture];
+                            foreach (var skill in data.CultureSkills[culture])
+                            {
+                                var skillKey = factMappings[skill];
+                                var advancementListFactKey =
+                                    db.Single<AdvancementListFact>(
+                                        "SELECT * FROM AdvancementListFacts WHERE FactId=@0 AND AdvancementListId=@1", skillKey,
+                                        (int)AdvancementListEnum.CultureSkills).Id;
+
+                                db.Insert(new AdvancementListFactModifier
+                                {
+                                    AdvancementListFactId = advancementListFactKey,
+                                    FactRequirementId = factKey
+                                });
+                            }
+                        }
+
+                        // This code is for adding the gates to the initial esoterics
+                        // list.
+                        foreach (var esoteric in data.EsotericGates.Keys)
+                        {
+                            var factKey = factMappings[esoteric];
+                            var advancementFactKey =
+                                db.Single<AdvancementListFact>(
+                                    "SELECT * FROM AdvancementListFacts WHERE FactId=@0 AND AdvancementListId=@1",
+                                    factKey, (int) AdvancementListEnum.Esoterics).Id;
+                            foreach (var requirement in data.EsotericGates[esoteric])
+                            {
+                                var requirementKey = factMappings[requirement];
+                                db.Insert(new AdvancementListFactGate
+                                {
+                                    AdvancementListFactId = advancementFactKey,
+                                    RequiredFactId = requirementKey
+                                });
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewBag.ValidationError = e.Message;
+                    return View["Index.cshtml", model];
+                }
+
+                ViewBag.ValidationMessage = "Database repopulated successfully!";
+                return View["Index.cshtml", model];
+            };
+            Post["/clear_db"] = parameters =>
+            {
+                var model = this.Bind<BaseCyanielViewModel>();
+
+                try
+                {
+                    using (var db = dbFactory.Create())
+                    {
+                        var tables = db.Fetch<string>("SELECT table_name FROM information_schema.tables " +
+                                                      "WHERE table_schema = 'larpworks';");
+                        foreach (var table in tables)
+                        {
+                            db.Execute("SET FOREIGN_KEY_CHECKS = 0; TRUNCATE TABLE " + table + "; SET FOREIGN_KEY_CHECKS = 1;");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewBag.ValidationError = e.Message;
+                    return View["Index.cshtml", model];
+                }
+
+                ViewBag.ValidationMessage = "Database cleared!";
+                return View["Index.cshtml", model];
+            };
+            Post["/random_character"] = parameters =>
+            {
+                var model = this.Bind<BaseCyanielViewModel>();
+
+                ViewBag.ValidationMessage = "New character generated with ID: " + 0;
+                return View["Index.cshtml", model];
+            };
+        }
+    }
+#endif
+}
